903ad7879b1ecf2f960f66e6973f4b8433290978
Troy <hill.troy@gmail.com>
2018-11-29 15:30:27 -0500

    suppressWarnings in test

--- tests/testthat/test-getfirehydro.R
+++ tests/testthat/test-getfirehydro.R
@@ -1,9 +1,9 @@
 context("test-getfirehydro")
 
 test_that("getFireHydro works", {
-  outputDat <- getFireHydro(EDEN_date = "20181018", 
+  suppressWarnings(outputDat <- getFireHydro(EDEN_date = "20181018", 
                EDEN_GIS_directory = "EDEN_shp_20181018",
-               returnShp = TRUE)
+               returnShp = TRUE))
                      
   expect_equal(typeof(outputDat), "list")
   expect_equal(nrow(outputDat), 1533)

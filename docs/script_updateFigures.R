57f45924272154ad907f15322cdb365a85f5932c
Troy Hill <Hill.Troy@gmail.com>
2019-08-02 14:04:44 -0400

    R script to accompany github pages

---
+++ docs/script_updateFigures.R
@@ -0,0 +1,21 @@
+### script to generate most recent fireHydro maps
+
+if (!"devtools" %in% installed.packages()) {
+  install.packages("devtools")
+}
+  
+if (!"fireHydro" %in% installed.packages()) {
+  devtools::install_github("troyhill/fireHydro")
+}
+
+library(fireHydro)
+
+edenDat       <- getEDEN()
+fireDat <- getFireHydro(EDEN_date         = edenDat$date, 
+                       EDEN_GIS_directory = edenDat$data,
+                       fireSpreadExport   = paste0(getwd(), "/docs/figures/fireRisk.png"), 
+                       waterLevelExport   = paste0(getwd(), "/docs/figures/waterLevels.png"))
+
+### export output as a shapefile
+# st_write(fireDat, paste0(tempdir(), "/fireDat.shp"),
+#          driver = "ESRI Shapefile")

6e8a6b7f071576873f74a4b1e6e5b3f28f334c9e
troyhill <hill.troy@gmail.com>
2022-04-08 14:32:14 -0400

    Figuring out S3 methods

--- R/merge_EDEN.R
+++ R/merge_EDEN.R
@@ -2,7 +2,8 @@
 #'
 #' @description Merge EDEN objects (lists with data and dates)
 #' 
-#' @param x  list of EDEN objects
+#' @param x  EDEN object
+#' @param y  EDEN object
 #' 
 #' @return a single `eden` object, which is a list with two elements: (1) the date(s) used, and (2) a spatial object with water levels (centimeters relative to soil surface) in the EDEN grid.
 #' 
@@ -25,32 +26,34 @@
 #' @importFrom raster stack
 #' @importFrom terra  subset
 #' 
+#' @rdname eden-methods
 #' @export
 
 
-merge.eden <- function(x) {
+merge.eden <- function(x, y, ...) {
+  inputList <- list(x, y, ...)
   
-  if (!any(grepl(x = sapply(x, class), pattern = 'eden|list'))) {
-    stop('x must be a list of eden objects\n')
+  if (!any(grepl(x = sapply(inputList, class), pattern = 'eden|list'))) {
+    stop('arguments must be a list of eden objects\n')
   }
   
-  dateVec  <- Reduce(c, sapply(x, FUN = '[', 1))
+  dateVec  <- Reduce(c, sapply(inputList, FUN = '[', 1))
   ### will be sorting date and data elements by date
   newOrder <- order(dateVec)
   date_new <- dateVec[newOrder]
   
-  if (grepl(x = tolower(class(x[[1]]$data)), pattern = 'raster')) {
-    data_new <- Reduce(raster::stack, sapply(x, FUN = '[', 2))
+  if (grepl(x = tolower(class(inputList[[1]]$data)), pattern = 'raster')) {
+    data_new <- Reduce(raster::stack, sapply(inputList, FUN = '[', 2))
     data_new <- raster::subset(x = data_new, subset = newOrder)
   }
-  if (grepl(x = tolower(class(x[[1]]$data)), pattern = 'spatraster')) {
+  if (grepl(x = tolower(class(inputList[[1]]$data)), pattern = 'spatraster')) {
     # x2 <- list(list(date = eden2022$date[1:2], data = subset(eden2022$data, 1:2)),
     #            list(date = eden2022$date[3:4], data = subset(eden2022$data, 3:4))
     #            )
     # x2[[1]]$data <- terra::rast(x2[[1]]$data*1)
     # x2[[2]]$data <- terra::rast(x2[[2]]$data*1)
     # c(x2[[1]]$data, x2[[2]]$data )
-    data_new <- Reduce(c, sapply(x, FUN = '[', 2))
+    data_new <- Reduce(c, sapply(inputList, FUN = '[', 2))
     data_new <- terra::subset(x = data_new, subset = newOrder)
   }
   EDEN_list <- list(date = date_new,
--- R/plot_EDEN.R
+++ R/plot_EDEN.R
@@ -2,7 +2,8 @@
 #'
 #' @description Plot EDEN objects (lists with data and dates)
 #' 
-#' @param x  EDEN objects
+#' @param x  EDEN object
+#' @param y  NULL; not used
 #' 
 #' @return  a plot
 #' 
@@ -19,23 +20,27 @@
 #' }
 #' 
 #' @importFrom terra plot
-#' @importFrom terra subset
+#' @importFrom terra  subset
 #' 
 #' @export
 
 
-plot.eden <- function(x, subset = 1, ...) {
+plot.eden <- function(x, y = NULL, ...) {
   
+  sub <- 1
   if (!any(grepl(x = class(x), pattern = 'eden|list'))) {
     stop('x must be a list of eden objects\n')
   }
   
   
   if (any(grepl(x = tolower(class(x$data)), pattern = 'sf'))) {
-    ### quarterly data aren't available as sf objects, so subset isn't used here
+    ### quarterly data aren't available as sf objects, so sub isn't used here
     plot(x$data, border = NA, ...) # sf::plot_sf not working?
   }
+  if (any(!grepl(x = tolower(class(x$data)), pattern = 'spatraster'))) {
+    x$data[[sub]] <- terra::rast(x$data[[sub]])
+  }
   if (any(grepl(x = tolower(class(x$data)), pattern = 'spatraster'))) {
-    terra::plot(x$data[[subset]], main = x$date[[subset]], axes = FALSE, ...)
+    terra::plot(x$data[[sub]], main = x$date[[sub]], axes = FALSE, ...)
   }
 }
